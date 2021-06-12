dad1886da1e5c90b0cf4f958d63e543cce30eb3d
marcelonieva7 <marcelonieva7@gmail.com>
2021-06-12 01:26:38 -0300

    config env

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import reportWebVitals from './reportWebVitals';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOkdRo1pwzEaRbz1KpjMFeylG5mCdFNM8",
-  authDomain: "antimo-store.firebaseapp.com",
-  projectId: "antimo-store",
-  storageBucket: "antimo-store.appspot.com",
-  messagingSenderId: "891521205635",
-  appId: "1:891521205635:web:fd7ef0bb083eb8188a62b6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
 }
 
 ReactDOM.render(
